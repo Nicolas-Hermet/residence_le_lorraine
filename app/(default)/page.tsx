fa6369f87f2fc7501ddcfe14e5b61de2f70aecb0
Nicolas Hermet <nicolas.hermet@lisaia.com>
2025-04-12 16:05:46 +0200

    Change metadata

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,17 +1,14 @@
 export const metadata = {
-  title: 'Home - Creative',
-  description: 'Page description',
+  title: 'Résidence Le Lorraine - Hébergements Courte Durée et Cures Thermales',
+  description: 'Présentation des logements, tout ce que vous devez savoir avant de réserver votre logement pour une cure à La Bourboule',
 }
 
 import Hero from '@/components/hero'
 import Description from '@/components/description'
 import Inspiration from '@/components/inspiration'
 import Carousel from '@/components/carousel'
-import Creatives from '@/components/creatives'
 import Pricing from '@/components/pricing'
-import Testimonials from '@/components/testimonials'
 import Faqs from '@/components/faqs'
-import Blog from '@/components/blog'
 import Cta from '@/components/cta'
 
 export default function Home() {
