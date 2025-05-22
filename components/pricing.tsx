7f735c7924510ab7f61637c985d1a5f5b3fc2abb
Nicolas Hermet <nicolas.hermet@lisaia.com>
2025-05-22 14:35:31 +0200

    Add bullet points to pricing

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -46,6 +46,18 @@ export default function Pricing() {
                     <svg className="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
+                    <span>Une belle vue avec balcon ou calme sur cours.</span>
+                  </li>
+                  <li className="flex items-center">
+                    <svg className="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Emplacement d'exception</span>
+                  </li>
+                  <li className="flex items-center">
+                    <svg className="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
                     <span>Taxe de séjour pour 2 personnes (57€)</span>
                   </li>
                   <li className="flex items-center">
@@ -96,6 +108,18 @@ export default function Pricing() {
                 </div>
                 <div className="font-medium text-gray-100 mb-4">Inclus dans le tarif</div>
                 <ul className="text-gray-400 space-y-3 grow">
+                  <li className="flex items-center">
+                    <svg className="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Une belle vue avec balcon ou calme sur cours.</span>
+                  </li>
+                  <li className="flex items-center">
+                    <svg className="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Emplacement d'exception</span>
+                  </li>
                   <li className="flex items-center">
                     <svg className="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
