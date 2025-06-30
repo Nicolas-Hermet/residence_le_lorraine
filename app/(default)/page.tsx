0b0071dc42d9ff9c2a11fd0093598b6987af270c
Nicolas Hermet <nicolas.hermet@lisaia.com>
2025-06-30 16:53:22 +0200

    Blog Works... somehow. With openPro integration

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -10,7 +10,6 @@ import Carousel from '@/components/carousel'
 import Pricing from '@/components/pricing'
 import Faqs from '@/components/faqs'
 import Cta from '@/components/cta'
-import Blog from '@/components/blog'
 
 export default function Home() {
   return (
@@ -21,7 +20,6 @@ export default function Home() {
       <Carousel />
       <Pricing />
       <Faqs />
-      <Blog />
       <Cta />
     </>
   )
